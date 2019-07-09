c3f49f6b30fa667f219c43c1473c084c82b72add
吴彦祖 <zhanggx@ifeng.com>
2019-07-09 18:56:47 +0800

    update

--- src/main/java/com/ifeng/test/LogDemo.java
+++ src/main/java/com/ifeng/test/LogDemo.java
@@ -9,13 +9,24 @@ public class LogDemo {
     private static Logger blogger = LoggerFactory.getLogger("demo2");
     private static Logger clogger = LoggerFactory.getLogger("demo3");
 
+    private static Logger dlogger = LoggerFactory.getLogger(LogDemo.class);
+
+    private static final Logger logger = LoggerFactory.getLogger(LogDemo.class);
+
     public static void main(String[] args) {
-        alogger.info("aaaa");
-        blogger.info("bbbb");
-        clogger.info("cccc");
-        alogger.warn("a warning");
-        alogger.debug("a debug");
-        alogger.error("a error");
-        alogger.trace("a trace");
+//        alogger.info("aaaa");
+//        blogger.info("bbbb");
+//        clogger.info("cccc");
+//        alogger.warn("a warning");
+//        alogger.debug("a debug");
+//        alogger.error("a error");
+//        alogger.trace("a trace");
+//
+//        dlogger.info("d log");
+
+        logger.debug("debug");
+        logger.info("info");
+        logger.error("error");
+        logger.warn("warn");
     }
 }
